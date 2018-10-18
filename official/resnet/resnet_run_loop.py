1abfd25f6a4400127367fe9156eac661812d1a44
liupeng05 <liupeng05@kuaishou.com>
2018-10-18 11:20:24 +0800

    bug

--- official/resnet/resnet_run_loop.py
+++ official/resnet/resnet_run_loop.py
@@ -276,7 +276,54 @@ def resnet_model_fn(features, labels, mode, model_class,
 
     from official.resnet import resnet_model
 
-    model = resnet_model.Model(50, data_format='channels_last', resnet_version=1, dtype=tf.float32)
+    def _get_block_sizes(resnet_size):
+        """Retrieve the size of each block_layer in the ResNet model.
+
+        The number of block layers used for the Resnet model varies according
+        to the size of the model. This helper grabs the layer set we want, throwing
+        an error if a non-standard size has been selected.
+
+        Args:
+          resnet_size: The number of convolutional layers needed in the model.
+
+        Returns:
+          A list of block sizes to use in building the model.
+
+        Raises:
+          KeyError: if invalid resnet_size is received.
+        """
+        choices = {
+            18: [2, 2, 2, 2],
+            34: [3, 4, 6, 3],
+            50: [3, 4, 6, 3],
+            101: [3, 4, 23, 3],
+            152: [3, 8, 36, 3],
+            200: [3, 24, 36, 3]
+        }
+
+        try:
+            return choices[resnet_size]
+        except KeyError:
+            err = ('Could not find layers for selected Resnet size.\n'
+                   'Size received: {}; sizes allowed: {}.'.format(
+                resnet_size, choices.keys()))
+            raise ValueError(err)
+
+    model = resnet_model.Model(esnet_size=resnet_size,
+            bottleneck=True,
+            num_classes=num_classes,
+            num_filters=64,
+            kernel_size=7,
+            conv_stride=2,
+            first_pool_size=3,
+            first_pool_stride=2,
+            block_sizes=_get_block_sizes(resnet_size),
+            block_strides=[1, 2, 2, 2],
+            resnet_version=resnet_version,
+            data_format=data_format,
+            dtype=dtype
+        )
+
     logits = model(features, mode=tf.estimator.ModeKeys.TRAIN)
 
 
