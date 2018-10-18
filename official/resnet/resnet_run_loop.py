f7c88f1e89d0f88555965749220e66ca8fb1bc54
liupeng05 <liupeng05@kuaishou.com>
2018-10-18 14:36:56 +0800

    add regular loss

--- official/resnet/resnet_run_loop.py
+++ official/resnet/resnet_run_loop.py
@@ -484,19 +484,20 @@ def resnet_model_fn(features, labels, mode, model_class,
     tf.identity(cross_entropy, name='cross_entropy')
     tf.summary.scalar('cross_entropy', cross_entropy)
 
-    # If no loss_filter_fn is passed, assume we want the default behavior,
-    # which is that batch_normalization variables are excluded from loss.
-    def exclude_batch_norm(name):
-        return 'batch_normalization' not in name
-
-    loss_filter_fn = loss_filter_fn or exclude_batch_norm
-
-    # Add weight decay to the loss.
-    l2_loss = weight_decay * tf.add_n(
-        # loss is computed using fp32 for numerical stability.
-        [tf.nn.l2_loss(tf.cast(v, tf.float32)) for v in tf.trainable_variables()
-         if loss_filter_fn(v.name)])
-    tf.summary.scalar('l2_loss', l2_loss)
+    # # If no loss_filter_fn is passed, assume we want the default behavior,
+    # # which is that batch_normalization variables are excluded from loss.
+    # def exclude_batch_norm(name):
+    #     return 'batch_normalization' not in name
+    #
+    # loss_filter_fn = loss_filter_fn or exclude_batch_norm
+    #
+    # # Add weight decay to the loss.
+    # l2_loss = weight_decay * tf.add_n(
+    #     # loss is computed using fp32 for numerical stability.
+    #     [tf.nn.l2_loss(tf.cast(v, tf.float32)) for v in tf.trainable_variables()
+    #      if loss_filter_fn(v.name)])
+    # tf.summary.scalar('l2_loss', l2_loss)
+    l2_loss = tf.losses.get_regularization_loss()
     loss = cross_entropy + l2_loss
 
     if mode == tf.estimator.ModeKeys.TRAIN:
