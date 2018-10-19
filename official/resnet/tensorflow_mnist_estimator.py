997d856c45951e1e6f7976185e2746cb2d9d5eef
liupeng05 <liupeng05@kuaishou.com>
2018-10-19 20:04:32 +0800

    bug

--- official/resnet/tensorflow_mnist_estimator.py
+++ official/resnet/tensorflow_mnist_estimator.py
@@ -35,7 +35,7 @@ def cnn_model_fn(features, labels, mode):
     # Input Layer
     # Reshape X to 4-D tensor: [batch_size, width, height, channels]
     # MNIST images are 28x28 pixels, and have one color channel
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+    input_layer = tf.reshape(features, [-1, 28, 28, 1])
 
     # Convolutional Layer #1
     # Computes 32 features using a 5x5 filter with ReLU activation.
