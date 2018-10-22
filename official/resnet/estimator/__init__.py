c48c0464eada445ac2db1d433d731ca514e22d4b
liupeng05 <liupeng05@kuaishou.com>
2018-10-22 11:54:25 +0800

    bug

--- official/resnet/estimator/__init__.py
+++ official/resnet/estimator/__init__.py
@@ -22,4 +22,4 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow.python.estimator.estimator_lib
+import official.resnet.estimator.estimator_lib
