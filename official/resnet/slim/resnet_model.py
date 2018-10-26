9568b019f0851bffe03ad520624e707b3cb65e44
liupeng05 <liupeng05@kuaishou.com>
2018-10-26 10:36:13 +0800

    use resnet

--- official/resnet/slim/resnet_model.py
+++ official/resnet/slim/resnet_model.py
@@ -544,3 +544,95 @@ class Model(object):
             inputs = tf.layers.dense(inputs=inputs, units=self.num_classes)
             inputs = tf.identity(inputs, 'final_dense')
             return inputs
+
+
+def _get_block_sizes(resnet_size):
+    """Retrieve the size of each block_layer in the ResNet model.
+
+    The number of block layers used for the Resnet model varies according
+    to the size of the model. This helper grabs the layer set we want, throwing
+    an error if a non-standard size has been selected.
+
+    Args:
+      resnet_size: The number of convolutional layers needed in the model.
+
+    Returns:
+      A list of block sizes to use in building the model.
+
+    Raises:
+      KeyError: if invalid resnet_size is received.
+    """
+    choices = {
+        18: [2, 2, 2, 2],
+        34: [3, 4, 6, 3],
+        50: [3, 4, 6, 3],
+        101: [3, 4, 23, 3],
+        152: [3, 8, 36, 3],
+        200: [3, 24, 36, 3]
+    }
+
+    try:
+        return choices[resnet_size]
+    except KeyError:
+        err = ('Could not find layers for selected Resnet size.\n'
+               'Size received: {}; sizes allowed: {}.'.format(
+            resnet_size, choices.keys()))
+        raise ValueError(err)
+
+
+_DEFAULT_IMAGE_SIZE = 224
+_NUM_CHANNELS = 3
+_NUM_CLASSES = 1001
+
+_NUM_IMAGES = {
+    'train': 1281167,
+    'validation': 50000,
+}
+
+_NUM_TRAIN_FILES = 1024
+_SHUFFLE_BUFFER = 10000
+
+DATASET_NAME = 'ImageNet'
+
+
+class ResNet(Model):
+    """Model class with appropriate defaults for Imagenet data."""
+
+    def __init__(self, resnet_size, data_format=None, num_classes=_NUM_CLASSES,
+                 resnet_version=1,
+                 dtype=tf.float32):
+        """These are the parameters that work for Imagenet data.
+
+        Args:
+          resnet_size: The number of convolutional layers needed in the model.
+          data_format: Either 'channels_first' or 'channels_last', specifying which
+            data format to use when setting up the model.
+          num_classes: The number of output classes needed from the model. This
+            enables users to extend the same model to their own datasets.
+          resnet_version: Integer representing which version of the ResNet network
+            to use. See README for details. Valid values: [1, 2]
+          dtype: The TensorFlow dtype to use for calculations.
+        """
+
+        # For bigger models, we want to use "bottleneck" layers
+        if resnet_size < 50:
+            bottleneck = False
+        else:
+            bottleneck = True
+
+        super(ResNet, self).__init__(
+            resnet_size=resnet_size,
+            bottleneck=bottleneck,
+            num_classes=num_classes,
+            num_filters=64,
+            kernel_size=7,
+            conv_stride=2,
+            first_pool_size=3,
+            first_pool_stride=2,
+            block_sizes=_get_block_sizes(resnet_size),
+            block_strides=[1, 2, 2, 2],
+            resnet_version=resnet_version,
+            data_format=data_format,
+            dtype=dtype
+        )
+
