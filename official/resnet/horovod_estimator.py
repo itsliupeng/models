9753fd75bfd87d761ce4b2b9fb07fa3451260699
liupeng05 <liupeng05@kuaishou.com>
2018-10-26 16:17:14 +0800

    debug

--- official/resnet/horovod_estimator.py
+++ official/resnet/horovod_estimator.py
@@ -281,6 +281,9 @@ class HorovodEstimator(estimator.Estimator):
         worker_hooks.extend([
             # lp: loss hook
             # AllReduceTensorHook({'loss_avg': 'loss'}),
+            AllReduceTensorHook(
+                {"top1": 'train_accuracy', 'top5': 'train_accuracy_top_5', 'lr': 'learning_rate', 'loss': 'loss',
+                 'l2_loss': 'l2_loss', 'cross_entropy': 'cross_entropy'}, every_n_iter=100),
             training.NanTensorHook(estimator_spec.loss)
         ])
         if self._config.log_step_count_steps is not None:
