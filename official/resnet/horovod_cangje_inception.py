f4d609d6bc835155881f155f119b479dddf1e64c
liupeng05 <liupeng05@kuaishou.com>
2018-11-02 17:58:10 +0800

    bug

--- official/resnet/horovod_cangje_inception.py
+++ official/resnet/horovod_cangje_inception.py
@@ -235,8 +235,8 @@ def model_fn(features, labels, mode, params):
     lp_debug_rank0('regularization_losses size {}'.format(len(regularization_losses)))
 
     # l2_loss = weight_decay * tf.add_n([tf.nn.l2_loss(tf.cast(v, tf.float32)) for v in trainable_variables_without_bn])
-    l2_loss = regularization_losses
-    loss = tf.add_n([cross_entropy, aux_loss], l2_loss)
+    l2_loss = tf.add_n(regularization_losses)
+    loss = cross_entropy, aux_loss + l2_loss
 
     tf.identity(cross_entropy, name='cross_entropy')
     tf.identity(aux_loss, name='aux_loss')
