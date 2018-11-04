b768ac70221b8aba075216be4b8d2ba23226b6fb
liupeng05 <liupeng05@kuaishou.com>
2018-11-04 14:57:22 +0800

    bug

--- official/resnet/horovod_estimator.py
+++ official/resnet/horovod_estimator.py
@@ -341,7 +341,7 @@ class ConfusionMatrixHook(basic_session_run_hooks.SecondOrStepTimer, tf.train.Se
         all_predicts_np = np.concatenate(self._all_predicts)
         cnf_matrix = confusion_matrix(all_labels_np, all_predicts_np)
         classes = list(map(lambda x: str(x), range(0, self._num_classes)))
-        summary = self.confusion_matrix_summary(self.confusion_matrix_summary('confusion_matrix', cnf_matrix, classes))
+        summary = self.confusion_matrix_summary('confusion_matrix', cnf_matrix, classes)
         summary_writer.add_summary(summary, self._global_step)
 
 
