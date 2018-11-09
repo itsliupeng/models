0962c6f346196b47ceb42aae818f8d528ca1f9fb
liupeng05 <liupeng05@kuaishou.com>
2018-11-09 16:16:22 +0800

    dtype support except model

--- official/resnet/horovod_app_resnet.py
+++ official/resnet/horovod_app_resnet.py
@@ -36,40 +36,6 @@ def get_filenames(is_training, data_dir, test=False):
 
 
 def _parse_example_proto(example_serialized):
-    """Parses an Example proto containing a training example of an image.
-
-    The output of the build_image_data.py image preprocessing script is a dataset
-    containing serialized Example protocol buffers. Each Example proto contains
-    the following fields (values are included as examples):
-
-      image/height: 462
-      image/width: 581
-      image/colorspace: 'RGB'
-      image/channels: 3
-      image/class/label: 615
-      image/class/synset: 'n03623198'
-      image/class/text: 'knee pad'
-      image/object/bbox/xmin: 0.1
-      image/object/bbox/xmax: 0.9
-      image/object/bbox/ymin: 0.2
-      image/object/bbox/ymax: 0.6
-      image/object/bbox/label: 615
-      image/format: 'JPEG'
-      image/filename: 'ILSVRC2012_val_00041207.JPEG'
-      image/encoded: <JPEG encoded string>
-
-    Args:
-      example_serialized: scalar Tensor tf.string containing a serialized
-        Example protocol buffer.
-
-    Returns:
-      image_buffer: Tensor tf.string containing the contents of a JPEG file.
-      label: Tensor tf.int32 containing the label.
-      bbox: 3-D float Tensor of bounding boxes arranged [1, num_boxes, coords]
-        where each coordinate is [0, 1) and the coordinates are arranged as
-        [ymin, xmin, ymax, xmax].
-    """
-    # Dense features in Example proto.
     feature_map = {
         'image/encoded': tf.FixedLenFeature([], dtype=tf.string, default_value=''),
         'image/class/label': tf.FixedLenFeature([], dtype=tf.int64, default_value=-1),
@@ -103,20 +69,6 @@ def _parse_example_proto(example_serialized):
 
 
 def parse_record(raw_record, is_training, dtype):
-    """Parses a record containing a training example of an image.
-
-    The input record is parsed into a label and image, and the image is passed
-    through preprocessing steps (cropping, flipping, and so on).
-
-    Args:
-      raw_record: scalar Tensor tf.string containing a serialized
-        Example protocol buffer.
-      is_training: A boolean denoting whether the input is for training.
-      dtype: data type to use for images/features.
-
-    Returns:
-      Tuple with processed image tensor and one-hot-encoded label tensor.
-    """
     image_buffer, label, bbox = _parse_example_proto(raw_record)
 
     image = imagenet_preprocessing.preprocess_image(
@@ -134,54 +86,22 @@ def parse_record(raw_record, is_training, dtype):
 
 def process_record_dataset(dataset, is_training, batch_size, shuffle_buffer,
                            parse_record_fn, num_epochs=1, num_gpus=None,
-                           examples_per_epoch=None, dtype=tf.float32):
-    """Given a Dataset with raw records, return an iterator over the records.
-
-    Args:
-      dataset: A Dataset representing raw records
-      is_training: A boolean denoting whether the input is for training.
-      batch_size: The number of samples per batch.
-      shuffle_buffer: The buffer size to use when shuffling records. A larger
-        value results in better randomness, but smaller values reduce startup
-        time and use less memory.
-      parse_record_fn: A function that takes a raw record and returns the
-        corresponding (image, label) pair.
-      num_epochs: The number of epochs to repeat the dataset.
-      num_gpus: The number of gpus used for training.
-      examples_per_epoch: The number of examples in an epoch.
-      dtype: Data type to use for images/features.
-
-    Returns:
-      Dataset of (image, label) pairs ready for iteration.
-    """
-
-    # Prefetches a batch at a time to smooth out the time taken to load input
-    # files for shuffling and processing.
+                           examples_per_epoch=None, dtype=tf.float16):
     dataset = dataset.prefetch(buffer_size=batch_size)
     if is_training:
-        # Shuffles records before repeating to respect epoch boundaries.
         dataset = dataset.shuffle(buffer_size=shuffle_buffer)
 
-    # Repeats the dataset for the number of epochs to train.
     dataset = dataset.repeat(num_epochs)
-
-    # Parses the raw records into images and labels.
     dataset = dataset.apply(tf.contrib.data.map_and_batch(lambda value: parse_record_fn(value, is_training, dtype),
             batch_size=batch_size,
             # num_parallel_calls=1,
             drop_remainder=False))
 
-    # Operations between the final prefetch and the get_next call to the iterator
-    # will happen synchronously during run time. We prefetch here again to
-    # background all of the above processing work and keep it out of the
-    # critical training path. Setting buffer_size to tf.contrib.data.AUTOTUNE
-    # allows DistributionStrategies to adjust how many batches to fetch based
-    # on how many devices are present.
     dataset = dataset.prefetch(buffer_size=tf.contrib.data.AUTOTUNE)
     return dataset
 
 
-def input_fn(is_training, data_dir, batch_size, num_epochs=1, num_gpus=None, dtype=tf.float32, num_shards=1, shard_index=0, test=False, seed=None):
+def input_fn(is_training, data_dir, batch_size, num_epochs=1, num_gpus=None, dtype=tf.float16, num_shards=1, shard_index=0, test=False, seed=None):
     filenames = get_filenames(is_training, data_dir, test=test)
     dataset = tf.data.Dataset.from_tensor_slices(filenames)
 
@@ -210,6 +130,10 @@ def input_fn(is_training, data_dir, batch_size, num_epochs=1, num_gpus=None, dty
 
 
 def model_fn_label_smoothing(features, labels, mode, params):
+    dtype = params['dtype']
+
+    assert features.dtype == dtype
+
     if flags_obj.label_smoothing:
         lp_debug_rank0('using label smoothing')
         eta = 0.1
@@ -317,91 +241,6 @@ def model_fn_label_smoothing(features, labels, mode, params):
         metrics = {'val_accuracy': accuracy, 'val_accuracy_top_5': accuracy_top_5}
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, loss=loss, eval_metric_ops=metrics)
 
-def model_fn(features, labels, mode, params):
-    model = nets_factory.get_network_fn(flags_obj.model_type, flags_obj.num_classes, is_training=mode == tf.estimator.ModeKeys.TRAIN)
-    logits, end_points = model(features)
-    logits = tf.cast(logits, tf.float32)
-
-    predicts = tf.argmax(input=logits, axis=1)
-
-    tf.identity(features, 'features')
-    tf.identity(labels, 'labels')
-    tf.identity(predicts, 'predicts')
-
-    predictions = {
-        "classes": tf.argmax(input=logits, axis=1),
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-    cross_entropy = tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels, weights=1.0)
-
-    if 'AuxLogits' in end_points:
-        aux_logits = end_points['AuxLogits']
-        aux_logits = tf.cast(aux_logits, tf.float32)
-        aux_loss = tf.losses.sparse_softmax_cross_entropy(logits=aux_logits, labels=labels, weights=0.4)
-
-    else:
-        aux_loss = tf.constant(0.0)
-
-    def exclude_batch_norm_and_bias(name):
-        return 'batch_normalization' not in name and 'BatchNorm' not in name and 'biases' not in name
-
-    trainable_variables = tf.trainable_variables()
-    trainable_variables_without_bn = [v for  v in tf.trainable_variables() if exclude_batch_norm_and_bias(v.name)]
-    global_variables = tf.global_variables()
-
-    regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-
-    lp_debug_rank0('global_variables {}'.format(len(global_variables)))
-    lp_debug_rank0('trainable_variables {}'.format(len(trainable_variables)))
-    lp_debug_rank0('trainable_variables_without_bn size {}'.format(len(trainable_variables_without_bn)))
-    lp_debug_rank0('regularization_losses size {}'.format(len(regularization_losses)))
-
-    l2_loss = flags_obj.weight_decay * tf.add_n([tf.nn.l2_loss(tf.cast(v, tf.float32))
-                                                 for v in trainable_variables_without_bn])
-    loss = cross_entropy + aux_loss + l2_loss
-
-    tf.identity(cross_entropy, name='cross_entropy')
-    tf.identity(aux_loss, name='aux_loss')
-    tf.identity(l2_loss, 'l2_loss')
-    tf.identity(loss, name='loss')
-
-    accuracy = tf.metrics.accuracy(labels, predictions['classes'])
-    accuracy_top_5 = tf.metrics.mean(tf.nn.in_top_k(predictions=logits, targets=labels, k=5, name='top_5_op'))
-
-    tf.identity(accuracy[1], name='train_accuracy')
-    tf.identity(accuracy_top_5[1], name='train_accuracy_top_5')
-
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        global_step = tf.train.get_or_create_global_step()
-
-        learning_rate = params['learning_rate_fn'](global_step)
-        optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
-        optimizer = hvd.DistributedOptimizer(optimizer)
-
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            avg_grad_vars = optimizer.compute_gradients(loss)
-            minimize_op = optimizer.apply_gradients(avg_grad_vars, global_step)
-
-        train_op = tf.group(minimize_op, update_ops)
-
-        tf.identity(learning_rate, name='learning_rate')
-        lp_debug_rank0('update_ops size {}'.format(len(update_ops)))
-
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-    else:
-        if hvd.rank() == 0:
-            tf.identity(accuracy[1], name='eval_accuracy')
-            tf.identity(accuracy_top_5[1], name='eval_accuracy_top_5')
-            tf.summary.scalar('eval_accuracy', accuracy[1])
-            tf.summary.scalar('eval_accuracy_top_5', accuracy_top_5[1])
-
-        metrics = {'val_accuracy': accuracy, 'val_accuracy_top_5': accuracy_top_5}
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, loss=loss, eval_metric_ops=metrics)
-
 
 def main(unused_argv):
     hvd.init()
@@ -433,28 +272,37 @@ def main(unused_argv):
     #     num_images=flags_obj.num_images, boundary_epochs=[30, 60, 80, 90],
     #     decay_rates=[1, 0.1, 0.01, 0.001, 1e-4], warmup=True, base_lr=flags_obj.base_lr)
 
+    DTYPE_MAP = {'fp16': tf.float16, 'fp32': tf.float32 }
+    dtype = DTYPE_MAP[flags_obj.dtype]
+
     model_dir = flags_obj.model_dir if hvd.rank() == 0 else None
     classifier = HorovodEstimator(model_fn=model_fn_label_smoothing, model_dir=model_dir,
                                   config=tf.estimator.RunConfig(session_config=session_config, save_checkpoints_steps=flags_obj.save_checkpoints_steps),
-                                  params={'learning_rate_fn': learning_rate_fn})
+                                  params={'learning_rate_fn': learning_rate_fn, 'dtype': dtype})
 
-    def input_fn_train(num_epochs):
+    def input_fn_train(num_epochs, dtype=tf.float16):
         return input_fn(
             is_training=True, data_dir=flags_obj.data_dir,
             batch_size=flags_obj.batch_size,
-            num_epochs=num_epochs, num_shards=hvd.size(), shard_index=hvd.rank())
+            num_epochs=num_epochs,
+            dtype=dtype,
+            num_shards=hvd.size(),
+            shard_index=hvd.rank())
 
-    def input_fn_eval():
+    def input_fn_eval(dtype=tf.float16):
         return input_fn(
             is_training=False, data_dir=flags_obj.data_dir,
             batch_size=flags_obj.batch_size,
-            num_epochs=1)
+            num_epochs=1,
+            dtype=dtype)
 
-    def input_fn_test():
+    def input_fn_test(dtype=tf.float16):
         return input_fn(
             is_training=False, data_dir=flags_obj.data_dir,
             batch_size=flags_obj.batch_size,
-            num_epochs=1, test=True)
+            num_epochs=1,
+            dtype=dtype,
+            test=True)
 
     tensors_to_log = {"top1": 'train_accuracy', 'top5': 'train_accuracy_top_5', 'rmse': 'rmse', 'lr': 'learning_rate', 'loss': 'loss', 'l2_loss': 'l2_loss', 'cross_entropy': 'cross_entropy', 'aux_loss': 'aux_loss'}
     all_reduce_hook = AllReduceTensorHook(tensors_to_log, model_dir)
@@ -472,7 +320,7 @@ def main(unused_argv):
 
         if hvd.rank() == 0:
             lp_debug('begin evaluate')
-            eval_results = classifier.evaluate(input_fn=input_fn_eval, hooks=val_hooks)
+            eval_results = classifier.evaluate(input_fn=lambda: input_fn_eval(dtype=dtype), hooks=val_hooks)
             lp_debug(eval_results)
             lp_debug('end evaluate')
         else:
@@ -486,7 +334,7 @@ def main(unused_argv):
 
         if hvd.rank() == 0:
             lp_debug('begin test')
-            eval_results = classifier.evaluate(input_fn=input_fn_test, hooks=test_hooks)
+            eval_results = classifier.evaluate(input_fn=lambda: input_fn_test(dtype=dtype), hooks=test_hooks)
             lp_debug(eval_results)
             lp_debug('end test')
         else:
@@ -512,7 +360,7 @@ def main(unused_argv):
             else:
                 train_hooks.append(init_hooks)
 
-            classifier.train(input_fn=lambda: input_fn_train(num_train_epochs),
+            classifier.train(input_fn=lambda: input_fn_train(num_train_epochs, dtype=dtype),
                              hooks=train_hooks, max_steps=None)
 
             if hvd.rank() == 0:
@@ -521,7 +369,7 @@ def main(unused_argv):
                 if flags_obj.confusion_matrix:
                     val_hooks.append(cm_hook)
 
-                eval_results = classifier.evaluate(input_fn=input_fn_eval, hooks=val_hooks)
+                eval_results = classifier.evaluate(input_fn=lambda: input_fn_eval(dtype=dtype), hooks=val_hooks)
                 lp_debug(eval_results)
                 lp_debug('end evaluate')
             else:
@@ -554,6 +402,7 @@ if __name__ == "__main__":
     parser.add_argument('--confusion_matrix', help='', action='store_true')
     parser.add_argument('--num_images', help='', type=int, default=1281167)
     parser.add_argument('--weight_decay', help='', type=float, default=1e-4)
+    parser.add_argument('--dtype', help='', type=float, default='fp32')
 
     flags_obj = parser.parse_args()
 
